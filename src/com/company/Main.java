7ca3b4934afaecb2c02c016e87fc546729e93962
LefterStud <101552997+LefterStud@users.noreply.github.com>
2022-04-16 19:05:33 +0300

    Task 1 completed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,68 @@
 package com.company;
 
-import javax.swing.*;
+import static com.company.Stock.Max;
 
+/**
+ * Lefter Olexii, 18
+ */
 public class Main {
+    /**
+     * Определить самый дорогой товар на складе и напечатать все сведения о нем.
+     * (Количество, цена, год изготовления, производитель)
+     */
+    public static void task1() {
+        Stock product1 = new Stock();
+        Stock product2 = new Stock();
+        Stock product3 = new Stock();
+        Stock product4 = new Stock();
+        Stock product5 = new Stock();
+        Stock[] products = new Stock[]{product1, product2, product3, product4, product5};
 
+        product1.setAmount((float) Math.random());
+        product1.setPrice((float) Math.random());
+        product1.setYear((float) Math.random());
+        product1.setManufacturer("Ukraine");
 
+        product2.setAmount((float) Math.random());
+        product2.setPrice((float) Math.random());
+        product2.setYear((float) Math.random());
+        product2.setManufacturer("China");
 
-    public static void main(String[] args) {
+        product3.setAmount((float) Math.random());
+        product3.setPrice((float) Math.random());
+        product3.setYear((float) Math.random());
+        product3.setManufacturer("Germany");
 
+        product4.setAmount((float) Math.random());
+        product4.setPrice((float) Math.random());
+        product4.setYear((float) Math.random());
+        product4.setManufacturer("USA");
+
+        product5.setAmount((float) Math.random());
+        product5.setPrice((float) Math.random());
+        product5.setYear((float) Math.random());
+        product5.setManufacturer("Japan");
+
+        for (int i = 0; i < products.length; i++) {
+            if (products[i].getPrice() == Max) {
+                System.out.print(products[i].Info());
+            }
+        }
     }
 
 
+    /**
+     * Вывести сведения о футболистах, старших 20-ти лет и забивающих за игру не менее 0,4 мяча.
+     * (Фамилия, дата рождения, амплуа, количество игр, количество забитых мячей, место рождения)
+     */
+    public static void task2() {
 
+    }
 
+    public static void main(String[] args) {
+        task1();
 
-
+    }
 
 
 }
--- src/com/company/Stock.java
+++ src/com/company/Stock.java
@@ -1,7 +1,42 @@
 package com.company;
 
+/**
+ * Определить самый дорогой товар на складе и напечатать все сведения о нем.
+ * (Количество, цена, год изготовления, производитель)
+ */
 public class Stock {
+    private int amount;
+    private float price;
+    private int year;
+    private String manufacturer;
+    public static float Max = 0;
 
+    public void setAmount(float amount) {
+        this.amount = (int) (amount * 1000);
+    }
+
+    public void setPrice(float price) {
+        this.price = (float) ((int) (price * 100000)) / 100;
+        if (this.price > Max) {
+            Max = this.price;
+        }
+    }
+
+    public void setYear(float year) {
+        this.year = (int) ((year * 22) + 2000);
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public String Info() {
+        return "Amount - " + amount + " pieces;\nPrice - " + price + " UAH;\nProduction year - " + year + ";\nManufacturer - " + manufacturer + ".";
+    }
 
 }
 
